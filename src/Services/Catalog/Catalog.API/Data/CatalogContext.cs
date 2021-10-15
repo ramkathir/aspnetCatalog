c7e8d2b6ac1836084e0aaf586e8981c272022984
Ram Ganesan Kathiresan <ram.kathiresan79@gmail.com>
2021-10-15 08:49:48 +0530

    catalog api basic changes implemented

---
+++ src/Services/Catalog/Catalog.API/Data/CatalogContext.cs
@@ -0,0 +1,21 @@
+﻿using Catalog.API.Entities;
+using MongoDB.Driver;
+using System;
+using Microsoft.Extensions.Configuration;
+
+namespace Catalog.API.Data
+{
+    public class CatalogContext : ICatalogContext
+    {
+        public CatalogContext(IConfiguration config)
+        {
+            var client = new MongoClient(config.GetValue<string>("DatabaseSettings:ConnectionString"));
+            var database = client.GetDatabase(config.GetValue<string>("DatabaseSettings:DatabaseName"));
+
+            Products = database.GetCollection<Product>(config.GetValue<string>("DatabaseSettings:CollectionName"));
+            CatalogContextSeed.SeedData(Products);
+        }
+
+        public IMongoCollection<Product> Products { get; }
+    }
+}
